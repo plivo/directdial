a5c6b783fe232b6dce1fd90fa4629afe16372675
Chandresh Tank <chandresh@plivo.com>
2022-12-21 19:00:46 +0530

    Adding changes for python3 support and easytrunk

--- python/app.py
+++ python/app.py
@@ -1,13 +1,15 @@
 import os
 import os.path
-from flask import Flask, request, make_response
-import plivoxml
+import traceback
+from flask import Flask, request, make_response, url_for
+import plivo
+import plivo.xml as plivoxml
 app = Flask(__name__)
 
 @app.route('/direct-dial/', methods=['GET', 'POST'])
 def sip_route():
     try:
-        print "SIP Route %s" % request.values.items()
+        print("SIP Route %s" % request.values.items())
         to = request.args.get('ForwardTo', None)
         _from = request.args.get('CLID', None)
         dial_music = request.args.get('DialMusic', "")
@@ -31,24 +33,24 @@ def sip_route():
             response = make_response("SIP Route hangup callback")
             return response
 
-        r = plivoxml.Response()
+        r = plivoxml.ResponseElement()
 
         if not to:
-            print "SIP Route cannot identify destination number"
-            r.addHangup()
+            print("SIP Route cannot identify destination number")
+            r.add_hangup()
         else:
             if to[:4] == 'sip:':
                 is_sip_user = True
             else:
                 is_sip_user = False
             if is_sip_user and disable_call in ('all', 'sip'):
-                print "SIP Route calling sip user is disabled : %s" % str(disable_call)
-                r.addHangup(reason="busy")
+                print("SIP Route calling sip user is disabled : %s" % str(disable_call))
+                r.add_hangup(reason="busy")
             elif not is_sip_user and disable_call in ('all', 'number'):
-                print "SIP Route calling number is disabled : %s" % str(disable_call)
-                r.addHangup(reason="busy")
+                print("SIP Route calling number is disabled : %s" % str(disable_call))
+                r.add_hangup(reason="busy")
             else:
-                print "SIP Route dialing %s" % str(to)
+                print("SIP Route dialing %s" % str(to))
                 if not dial_music:
                     d = r.addDial(callerId=_from, callerName=cname)
                 else:
@@ -58,15 +60,16 @@ def sip_route():
                 else:
                     d.addNumber(to)
 
-        response = make_response(r.to_xml())
+        response = make_response(r.to_string())
         response.headers['Content-Type'] = 'text/xml'
         return response
 
-    except Exception, e:
-        print str(e)
+    except Exception as e:
+        print(str(e))
+        print(str(traceback.format_exc()))
         return "ERROR %s" % str(e)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
